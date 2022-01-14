f7dfc4153bc9023ffc2eb73707e9ee9a5c338ab9
prodampo4ky <32454745+prodampo4ky@users.noreply.github.com>
2022-01-14 13:39:34 +0200

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,208 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(80, 24);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(284, 129);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(462, 24);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(288, 129);
+            this.richTextBox2.TabIndex = 1;
+            this.richTextBox2.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(164, 369);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(95, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Зашифрувати";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(532, 369);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(107, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Розшифрувати";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(179, 320);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 4;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(179, 286);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 5;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(617, 290);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 6;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(617, 324);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 7;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(77, 293);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(20, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "A=";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(77, 327);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(20, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "B=";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(529, 293);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 13);
+            this.label3.TabIndex = 10;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(529, 327);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 13);
+            this.label4.TabIndex = 11;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(443, 293);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(20, 13);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "А=";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(443, 327);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(20, 13);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "B=";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,214 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string encrypted_text;
+        StringBuilder inputStringBuilder;
+        private Dictionary<string, double> frequency = new Dictionary<string, double>();
+        List<char> forbiddenCharacters = new List<char>()
+            { '.', ',', '!', '?', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '=', '+', '/', '\'', '\"', ':', ';', ' ', '’', '\n' };
+        public static List<char> alphavit = new List<char>() { 'а', 'б', 'в', 'г', 'ґ', 'д', 'е', 'є', 'ж', 'з', 'и', 'і', 'ї', 'й', 'к', 'л',
+            'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ь', 'ю', 'я' };
+        private readonly List<int> posibleAlpha = new List<int>() { 1, 2, 3, 4, 5, 7, 8, 10, 13, 14, 16, 17, 19, 20, 23, 25, 26, 28, 29, 31, 32 };
+        private readonly List<int> posibleBeta = new List<int>() { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15,
+            16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32 };
+
+        /*private void Aphine_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            Application.Exit();
+        }*/
+
+        /*private void button1_Click(object sender, EventArgs e)
+        {
+            richTextBox2.Text = encrypted_text;
+            if (string.IsNullOrEmpty(richTextBox1.Text))
+            {
+                MessageBox.Show("Text is missing");
+            }
+            inputStringBuilder = new StringBuilder(richTextBox1.Text.ToLower());
+            for (int i = 0; i < inputStringBuilder.Length; i++)
+            {
+                if (!alphavit.Contains(inputStringBuilder[i]))
+                {
+                    inputStringBuilder.Remove(i, 1);
+                    i--;
+                }
+            }
+
+            richTextBox2.Clear();
+
+            GetEncryption();
+
+        }*/
+
+        /*private void button2_Click(object sender, EventArgs e)
+        {
+
+            richTextBox2.Text = encrypted_text;
+
+            int count = 0;
+            inputStringBuilder = new StringBuilder(richTextBox1.Text);
+            for (int i = 0; i < inputStringBuilder.Length; i++)
+            {
+                count++;
+                var key = inputStringBuilder[i].ToString();
+                if (frequency.ContainsKey(key))
+                {
+                    frequency[key]++;
+                    continue;
+                }
+                frequency.Add(key, 1);
+            }
+
+            var twoCharWithMaxFrequency = frequency.OrderByDescending(f => f.Value).Select(f => f.Key).Take(2).ToList();
+
+            var aAndB = SolvingEquation(twoCharWithMaxFrequency);
+            if (aAndB == null)
+            {
+                MessageBox.Show("Неможливо розшифрувати, підберіть інші літери");
+            }
+
+            GetDecryption(aAndB);
+            label3.Text = $"{aAndB.Item1}";
+            label4.Text = $"{aAndB.Item2}";
+
+        }*/
+
+        private void GetEncryption()
+        {
+            int helper;
+            StringBuilder encryptedText = new StringBuilder();
+            for (int i = 0; i < inputStringBuilder.Length; i++)
+            {
+                helper = (alphavit.IndexOf(inputStringBuilder[i]) * Convert.ToInt32(textBox1.Text) + Convert.ToInt32(textBox2.Text)) % alphavit.Count;
+                encryptedText.Append(alphavit.ElementAt(helper));
+            }
+            richTextBox2.Text = encryptedText.ToString();
+        }
+
+        private Tuple<int, int> SolvingEquation(List<string> twoCharWithMaxFrequency)
+        {
+            int indexOfFirstChar = alphavit.IndexOf(twoCharWithMaxFrequency[0].ToCharArray()[0]);
+            int indexOfSecondChar = alphavit.IndexOf(twoCharWithMaxFrequency[1].ToCharArray()[0]);
+
+            int indexFirstMaxFreqChar = alphavit.IndexOf(textBox3.Text.ToCharArray()[0]);
+            int indexSecondMaxFreqChar = alphavit.IndexOf(textBox4.Text.ToCharArray()[0]);
+
+            for (int i = 0; i < posibleAlpha.Count; i++)
+            {
+                for (int j = 0; j < posibleBeta.Count; j++)
+                {
+                    var solution1 = (posibleAlpha[i] * indexFirstMaxFreqChar + posibleBeta[j]) % alphavit.Count == indexOfFirstChar;
+                    var solution2 = (posibleAlpha[i] * indexSecondMaxFreqChar + posibleBeta[j]) % alphavit.Count == indexOfSecondChar;
+
+                    if (solution1 && solution2)
+                    {
+                        return new Tuple<int, int>(posibleAlpha[i], posibleBeta[j]);
+                    }
+                }
+            }
+
+            return null;
+        }
+        private void GetDecryption(Tuple<int, int> aAndB)
+        {
+            int helper = 0;
+            StringBuilder decryptedText = new StringBuilder();
+            for (int i = 0; i < inputStringBuilder.Length; i++)
+            {
+                helper = modInverse(aAndB.Item1, alphavit.Count) * (alphavit.IndexOf(inputStringBuilder[i]) + alphavit.Count - aAndB.Item2) % alphavit.Count;
+                decryptedText.Append(alphavit.ElementAt(helper));
+            }
+            richTextBox2.Text = decryptedText.ToString();
+        }
+
+        private int modInverse(int a, int n)
+        {
+            int i = n, v = 0, d = 1;
+            while (a > 0)
+            {
+                int t = i / a, x = a;
+                a = i % x;
+                i = x;
+                x = d;
+                d = v - t * x;
+                v = x;
+            }
+            v %= n;
+            if (v < 0) v = (v + n) % n;
+            return v;
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            richTextBox2.Text = encrypted_text;
+            if (string.IsNullOrEmpty(richTextBox1.Text))
+            {
+                MessageBox.Show("Text is missing");
+            }
+            inputStringBuilder = new StringBuilder(richTextBox1.Text.ToLower());
+            for (int i = 0; i < inputStringBuilder.Length; i++)
+            {
+                if (!alphavit.Contains(inputStringBuilder[i]))
+                {
+                    inputStringBuilder.Remove(i, 1);
+                    i--;
+                }
+            }
+
+            richTextBox2.Clear();
+
+            GetEncryption();
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            richTextBox2.Text = encrypted_text;
+
+            int count = 0;
+            inputStringBuilder = new StringBuilder(richTextBox1.Text);
+            for (int i = 0; i < inputStringBuilder.Length; i++)
+            {
+                count++;
+                var key = inputStringBuilder[i].ToString();
+                if (frequency.ContainsKey(key))
+                {
+                    frequency[key]++;
+                    continue;
+                }
+                frequency.Add(key, 1);
+            }
+
+            var twoCharWithMaxFrequency = frequency.OrderByDescending(f => f.Value).Select(f => f.Key).Take(2).ToList();
+
+            var aAndB = SolvingEquation(twoCharWithMaxFrequency);
+            if (aAndB == null)
+            {
+                MessageBox.Show("Неможливо розшифрувати, підберіть інші літери");
+            }
+
+            GetDecryption(aAndB);
+            label3.Text = $"{aAndB.Item1}";
+            label4.Text = $"{aAndB.Item2}";
+        }
+    }
+}
